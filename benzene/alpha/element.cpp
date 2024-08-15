d1594736482e5986985ca2f457711ce838de2ee7
Punished Benzene <pcvtermin@gmail.com>
2024-08-15 03:22:25 +0300

    оптимизировал алгоритм фильтрации + нужна доработка для элементов >= 103

--- benzene/alpha/element.cpp
+++ benzene/alpha/element.cpp
@@ -22,13 +22,20 @@ electronic_configuration::electronic_configuration(const int charge){
 }
 
 electronic_configuration::operator std::string (){
+
+    return to_string(_electronic_configuration);
+
+}
+
+std::string electronic_configuration::to_string(std::vector<std::vector<int>> orbit) { 
     std::string out;
     std::vector<std::vector<int>>::iterator el_config_iterator;
 
     int n = 0;
     std::vector<int> current_orbit;
     int len;
-    for(el_config_iterator = _electronic_configuration.begin(); el_config_iterator < _electronic_configuration.end(); el_config_iterator++){
+
+    for(el_config_iterator = orbit.begin(); el_config_iterator < orbit.end(); el_config_iterator++){
 
         current_orbit = *el_config_iterator;
 
@@ -37,7 +44,7 @@ electronic_configuration::operator std::string (){
         switch (current_orbit.size())
         {
         case 1:
-            if (len == 2) {n++;};
+            n++;
             out += std::to_string(n) + "S" + std::to_string(len) + " ";
             break;
         case 3:
@@ -55,7 +62,8 @@ electronic_configuration::operator std::string (){
     }
 
     return out;
-}
+
+};
 
 element::element(const int charge): _electronic_configuration(charge) {
 
@@ -67,13 +75,20 @@ electronic_configuration element::get_electronic_configuration() {
     return _electronic_configuration;
 }
 
+std::vector<std::vector<int>> electronic_configuration::get_orbits() {
+
+    return _electronic_configuration;
+
+}
 
 
 std::vector<std::vector<int>> electronic_configuration::generate_configuration_stupid_method(int aviable_electrons){
     std::vector<std::vector<int>> result = std::vector<std::vector<int>>{};
+    std::vector<std::vector<int>>::iterator last_s_iterator;
 
     int nal = 0;
     int cash = aviable_electrons;
+    
     while (cash > 0)
     {
 
@@ -94,12 +109,10 @@ std::vector<std::vector<int>> electronic_configuration::generate_configuration_s
             
             float k = 100 - std::round((float(cash) / float(aviable_electrons) * 100.0));
 
-            std::vector<int> z;
             switch (l)
             {
             case 0:
-                z = electronic_configuration::generate_orbit(ENERGY_LEVEL::S_LEVEL, &cash);
-                result.emplace_back(z);
+                result.emplace_back(electronic_configuration::generate_orbit(ENERGY_LEVEL::S_LEVEL, &cash));
                 break;
             case 1:
                 result.emplace_back(electronic_configuration::generate_orbit(ENERGY_LEVEL::P_LEVEL, &cash));
@@ -121,54 +134,73 @@ std::vector<std::vector<int>> electronic_configuration::generate_configuration_s
     }
     
     //теперь фильтруем
+    int len;
+    std::vector<std::vector<int>>::iterator orbit_buffer;
+    std::vector<std::vector<int>>::iterator orbit_buffer_neded_iter;
+    for(std::vector<std::vector<int>>::iterator i = result.begin(); i < result.end(); i++) {
 
-    std::vector<std::vector<int>>::iterator result_iterator;
-    std::vector<std::vector<int>>::iterator next_iterator;
-    std::vector<int> vector_buffer;
-    std::vector<int> vector_buffer2;
-    int buffer;
-    int buffer2;
-    logger::make_message("Filtering element with charge " + std::to_string(aviable_electrons), MSG_TYPE::INFO);
-    for (result_iterator = result.begin(); result_iterator < result.end(); result_iterator++)
-    {
-        if (std::next(result_iterator) >= result.end()) {
-            continue;
+        if ((*i).size() == 1) {
+            last_s_iterator = i;
         }
-        next_iterator = std::next(result_iterator);
-        vector_buffer2 = *next_iterator;
-        vector_buffer2 = *result_iterator;
-        
-        // я знаю, что проблема может быть только с последней орбиталью, а так-же я знаю, что все проблемные элементы, являются d элементами, соответственно
-        if (vector_buffer.size() == 1 && vector_buffer2.size()==5) {
-            logger::make_message("HK", MSG_TYPE::INFO);
-            buffer = vector_buffer2.front();
-            buffer2 = std::accumulate(vector_buffer.begin(), vector_buffer.end(),0);
 
-            if (buffer2 + 2 == 10) {
-                //система может заполнить полностью уровень
-                buffer2+=2;
-                *result_iterator = std::vector<int>{0};
-            } else {
-                if (buffer2 != 10) {
-                    *result_iterator = std::vector<int>{1};
-                    buffer2++;
-                }
+    }
+
+    for (std::vector<std::vector<int>>::iterator i = last_s_iterator; i < result.end(); i++ ) {
+        if ((*i).size() == 5) {
+            orbit_buffer = i;
+
+            len = std::accumulate((*orbit_buffer).begin(), (*orbit_buffer).end(), 0);
+
+
+            if (len < (*orbit_buffer).size()) {
+                (*orbit_buffer)[len]++;
+            } else if ((*orbit_buffer).size() < len < ((*orbit_buffer).size() * 2))
+            {   
+                if ((len + (*last_s_iterator).front()) == ((*orbit_buffer).size() * 2)) { 
+
+                    (*orbit_buffer) = std::vector<int>((*orbit_buffer).size(), 2);
+                    (*last_s_iterator).front() = 0;
+                    break;
+                 }
+                (*orbit_buffer)[(len-5)]++;
             }
-            
-            
-            *(next_iterator) = electronic_configuration::generate_orbit(ENERGY_LEVEL::D_LEVEL, &buffer2);
-            
-
-            
-
-        } else {
-            continue;
+        
+            (*last_s_iterator).front()--;
         }
     }
+
     logger::make_message("Gen ended", MSG_TYPE::INFO);
 
 
     return result;
+
+}
+
+std::vector<std::vector<int>> electronic_configuration::return_last_orbit() {
+    
+    std::vector<std::vector<int>> last_orbit;
+    std::vector<std::vector<int>>::iterator last_s_pos;
+
+    bool reversed = false;
+
+    for (auto i = _electronic_configuration.begin(); i <= _electronic_configuration.end(); i++)   {
+        
+        if (( ( (std::vector<int>)(*i) ).size() == 1 )) {
+
+            last_s_pos = i;
+
+        }
+
+    }
+
+    for (auto i = last_s_pos; i < _electronic_configuration.end(); i++)   {
+       
+        last_orbit.emplace_back(*i);
+
+    }
+
+    return last_orbit;
+    
 
 }
 
@@ -223,9 +255,9 @@ std::vector<int> electronic_configuration::generate_orbit(electronic_configurati
         }
         break;
     case ENERGY_LEVEL::F_LEVEL:
-        if (*electrons >= 10) {
+        if (*electrons >= 14) {
             result = std::vector<int>{2, 2, 2, 2, 2, 2, 2};
-            *electrons -= 10;
+            *electrons -= 14;
             return result;
         } else {
             result = std::vector<int>{0, 0, 0, 0, 0, 0, 0};
--- benzeneSUT/src/main.cpp
+++ benzeneSUT/src/main.cpp
@@ -3,21 +3,16 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Gen from ";
-    int a;
-    int b;
-    std::cin >> a;
-    std::cout << "To ";
-    std::cin >> b;
+    int start = 10;
+    int end = 30;
+    srand(time(0));
+    int x = rand() % (end - start + 1) + start;
+    
+    element foo = element(103);
 
-    element foo = element(a);
-    for (int i = a + a; i < b; i++) {
-        foo = element(i);
-        logger::make_message(foo.get_electronic_configuration(), MSG_TYPE::INFO);
-    }
     
     
-    int k;
-    std::cin >> k;
+    logger::make_message(electronic_configuration::to_string(foo.get_electronic_configuration().get_orbits()), MSG_TYPE::INFO);
+
     return 0;
 }
